963bb4a02b5e3717ed9109b28e45c909817db4d6
lltheis <theis.laura.l@gmail.com>
2016-05-01 20:54:49 +0000

    add site files

---
+++ js/scripts.js
@@ -0,0 +1,94 @@
+// TWIITER
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+
+
+// DOCUMENT IS READY
+$(document).ready(function(){
+	/* alert("Document is ready!"); */
+	// work section
+	for(var i = 0; i < works.length; ++i ) {
+		$("#work").append("\
+			<div class='lg-col-2 col-md-3 col-sm-4 col-xs-6 project'>\
+				<a target='_blank' href=" + works[i].URL + " class='work-img'>\
+				<img class='img-responsive' src='" + works[i].pic + "'>\
+				<span class='info'><p class='project-label'>Project:</p> " + works[i].title + "</span>\
+				</a>\
+			</div>\
+	");
+		$(".work-img").mouseenter(function() {
+    	$(".info", this).show();
+		}).mouseleave(function() {
+    	$(".info", this).hide();
+	});
+/*		var images = $("#work img");
+		if(i%2 === 0){
+			$(images[i]).css("border", "2px solid DodgerBlue");
+		} else {
+			$(images[i]).css("border", "2px solid salmon");
+		};*/
+	};
+});
+
+
+// CLOSE NAV DROPDOWNS
+$(document).ready(function() {
+	$('.dropdown').dropit();
+});
+
+
+// TOOLTIP
+$(function () {
+	$('#item1').tooltip();
+});
+
+
+// POPOVERS FOR TOUCH SCREEN TOOLTIPS
+$(function () {
+	$('[data-toggle="popover"]').popover({trigger: 'hover click'});
+});
+
+
+// STELLAR      
+$('#contact-background').stellar();
+
+
+// KEYUP
+$(".message-box").on("keyup", function() {
+	console.log("keyup happened");
+	var comment = $(".message-box").val();
+	var charCount = $(".message-box").val().length;
+	console.log(charCount);
+	$("#char-count").html(charCount);
+	if(charCount > 50) {
+		$("#char-count").css("color", "red");
+	} else {
+		$("#char-count").css("color", "black");
+	};
+});
+
+
+// EVENT LISTENER SUBMIT BUTTON
+
+$("#button").on("click", function(){
+	var comment=$(".message-box").val();if(comment!="") {
+		$("#visible-comment").html("Thank you for your message: '"+comment+"'<br>I will get back to you soon!");
+		$("#visible-comment").css("color","black");
+		$(".message-box").hide();
+	} else {
+		$(".message-box").css("border","2px solid red");
+		$("#visible-comment").html("Please submit a message that is not empty!");
+		$("#visible-comment").css("color","red");
+	};
+	return false;
+});
+
+// GOOGLE MAP
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 43.6807, lng: -114.3637},
+    zoom: 13,
+    mapTypeId: google.maps.MapTypeId.TERRAIN
+  });
+}
+
---
+++ js/work.js
@@ -0,0 +1,34 @@
+// PROJECT ARRAY
+var works = [
+	{
+		title: "Software package implementation",
+		pic: "img/pencils.jpg",
+		URL: "http://home.localfoodmarketplace.com/"
+	},
+	{
+		title: "ProProfs documentation",
+		pic: "img/planning.jpg",
+		URL: "http://www.proprofs.com/knowledgebase/"
+	},
+	{
+		title: "JIRA Agile Systems Setup",
+		pic: "img/alphabet.jpg",
+		URL: "https://www.atlassian.com/software/jira"
+	},
+	{
+		title: "Streamlining new site templates",
+		pic: "img/writing.jpg",
+		URL: "https://www.localfoodmarketplace.com/centralmasslocavore/"
+	},
+	{
+		title: "Excel decision list tool",
+		pic: "img/lightbulb.jpg",
+		URL: "http://codes.iccsafe.org/Idaho.html"
+	},
+{
+		title: "Analysis, testing and implementation of new storefront",
+		pic: "img/rubics.jpg",
+		URL: "http://codes.iccsafe.org/Idaho.html"
+	},
+
+]
